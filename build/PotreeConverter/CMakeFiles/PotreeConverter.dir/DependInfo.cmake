70602b1a80fd2630eca572cd600f092422bc47c4
Nikitos1815 <chessmasters277@gmail.com>
2020-09-03 09:51:14 +0000

    Add buil directory

---
+++ build/PotreeConverter/CMakeFiles/PotreeConverter.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/BINPointReader.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/BINPointReader.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/GridCell.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/GridCell.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/LASPointReader.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/LASPointReader.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/LASPointWriter.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/LASPointWriter.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/PTXPointReader.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/PTXPointReader.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/PointAttributes.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/PointAttributes.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/PotreeConverter.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/PotreeConverter.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/PotreeWriter.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/PotreeWriter.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/SparseGrid.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/SparseGrid.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/main.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/main.cpp.o"
+  "/usr/local/src/PotreeConverter2/master/PotreeConverter/src/stuff.cpp" "/usr/local/src/PotreeConverter2/master/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/stuff.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../PotreeConverter/include"
+  "/usr/local/src/lastools/master/LASzip/dll"
+  "../PotreeConverter/lib/rapidjson/include"
+  "../PotreeConverter/lib/arguments"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
